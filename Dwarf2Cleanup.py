27ac06c1c3fa8c434c77500e2a6c7ac714954f4b
Dawson69 <mark.dawson08@gmail.com>
2023-07-26 16:34:49 +0100

    Removed test dir

--- Dwarf2Cleanup.py
+++ Dwarf2Cleanup.py
@@ -2,15 +2,45 @@
 import os
 import tkinter.filedialog as tk
 
+def doesProcessDirExist(path):
+    if os.path.isdir(path+'/process'):
+       return True
+    else:
+        return False
+
+def removeProcessDir(path):
+    processpath = path + "/Process"
+    files = os.listdir(processpath)
+    for f in files:
+        os.remove(processpath+'/'+f)
+    os.rmdir(path+"/Process")
+
+
+def iterateDirectories(path):
+    dirlist = os.listdir(path)
+    for dir in dirlist:
+        if os.path.isdir(path + '/' + dir):
+            if doesProcessDirExist(path+'/'+dir):
+                print('remove process dir '+path+'/'+dir)
+                removeProcessDir(path+'/'+dir)
+            else:
+                iterateDirectories(path+'/'+dir)
+
+
+
 filepath = tk.askdirectory(initialdir="D:\\Astronomy")
+files = os.listdir(filepath)
 
-path = filepath+"/Process"
+for f in files:
+    if os.path.isdir(filepath + '/' + f):
+        if doesProcessDirExist(filepath +'/' + f):
+            print('remove directory '+filepath +'/' + f)
+            removeProcessDir(filepath+'/'+dir)
+        else:
+            iterateDirectories(filepath +'/' + f)
 
-if not os.path.isdir(path):
+if not doesProcessDirExist(filepath):
     print("directory does not exist")
 else:
-    files = os.listdir(path)
-    for f in files:
-        os.remove(path+'/'+f)
-
-    os.rmdir(filepath+"/Process")
+    print('remove directory')
+    removeProcessDir(filepath)
